503a2b9f061d8c12bd810ead095360d74afe8fae
Prakhyat Singh <professionalgamersss@gmail.com>
2024-09-24 13:52:07 +0530

    changes

--- TreasureSail2/app/src/main/java/com/example/treasuresail/MainActivity.kt
+++ TreasureSail2/app/src/main/java/com/example/treasuresail/MainActivity.kt
@@ -7,18 +7,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.treasuresail.ui.theme.TreasureSailTheme
+import kotlin.math.max
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -40,59 +46,138 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TreasureHunt(){
+    var hp by remember{ mutableStateOf(5)}
+    var highScore by remember { mutableStateOf(0) }
+    var currentScore by remember { mutableStateOf(0)}
     val TreasureCount=remember{mutableStateOf(0)}
     val Direction=remember{ mutableStateOf("North")}
     val context= LocalContext.current
     val stormOrTreasure = remember { mutableStateOf("") }
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
-        Text(text = "Treasure Found: ${TreasureCount.value}")
-        Text(text = "Current Direction: ${Direction.value}")
-        Button(onClick = {Direction.value="West"
-            if(Random.nextBoolean()){
-                Toast.makeText(context,"Treasure Found!",Toast.LENGTH_SHORT).show()
-                TreasureCount.value+=1
-            }
-            else{
-                Toast.makeText(context,"Ran into a Storm!",Toast.LENGTH_SHORT).show()
-            }}) {
-            Text(text = "Sail East")
+    var showReset by remember {
+        mutableStateOf(false)
+    }
+    if(!showReset) {
+        if (hp == 0) {
+            Toast.makeText(context, "GAME OVER!", Toast.LENGTH_SHORT).show()
+            highScore = max(highScore, TreasureCount.value)
+            currentScore = TreasureCount.value
+            showReset = true
         }
-        Button(onClick = {Direction.value="West"
-            if(Random.nextBoolean()){
-                Toast.makeText(context,"Treasure Found!",Toast.LENGTH_SHORT).show()
-                TreasureCount.value+=1
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Hp: $hp",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Text(
+                text = "HighScore: $highScore",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Text(
+                text = "Treasure Found: ${TreasureCount.value}",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Text(
+                text = "Current Direction: ${Direction.value}",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Button(onClick = {
+                Direction.value = "East"
+                if (Random.nextBoolean()) {
+                    Toast.makeText(context, "Treasure Found!", Toast.LENGTH_SHORT).show()
+                    TreasureCount.value += 1
+                } else {
+                    if (hp > 0) {
+                        Toast.makeText(context, "Ran into a Storm!", Toast.LENGTH_SHORT).show()
+                        hp--
+                    }
+
+                }
+            }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Sail East")
             }
-        else{
-                Toast.makeText(context,"Ran into a Storm!",Toast.LENGTH_SHORT).show()
-        }}) {
-            Text(text = "Sail West")
+            Button(onClick = {
+                Direction.value = "West"
+                if (Random.nextBoolean()) {
+                    Toast.makeText(context, "Treasure Found!", Toast.LENGTH_SHORT).show()
+                    TreasureCount.value += 1
+                } else {
+                    if (hp > 0) {
+                        Toast.makeText(context, "Ran into a Storm!", Toast.LENGTH_SHORT).show()
+                        hp--
+                    }
+
+                }
+            }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Sail West")
+            }
+            Button(onClick = {
+                Direction.value = "South"
+                if (Random.nextBoolean()) {
+                    Toast.makeText(context, "Treasure Found!", Toast.LENGTH_SHORT).show()
+                    TreasureCount.value += 1
+                } else {
+                    if (hp > 0) {
+                        Toast.makeText(context, "Ran into a Storm!", Toast.LENGTH_SHORT).show()
+                        hp--
+                    }
+
+                }
+            }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Sail South")
+            }
+            Button(onClick = {
+                Direction.value = "North"
+                if (Random.nextBoolean()) {
+                    Toast.makeText(context, "Treasure Found!", Toast.LENGTH_SHORT).show()
+                    TreasureCount.value += 1
+                } else {
+                    if (hp > 0) {
+                        Toast.makeText(context, "Ran into a Storm!", Toast.LENGTH_SHORT).show()
+                        hp--
+                    }
+
+                }
+            }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Sail North")
+            }
         }
-        Button(onClick = {Direction.value="West"
-            if(Random.nextBoolean()){
-                Toast.makeText(context,"Treasure Found!",Toast.LENGTH_SHORT).show()
-                TreasureCount.value+=1
+    }
+    else{
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text(
+                text = "HighScore: $highScore",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Text(
+                text = "Your Score: $currentScore",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Button(onClick = {
+                hp = 5
+                TreasureCount.value = 0
+                showReset = false
+            }, modifier = Modifier.wrapContentSize()) {
+                Text(text = "Reset")
             }
-            else{
-                Toast.makeText(context,"Ran into a Storm!",Toast.LENGTH_SHORT).show()
-            }}) {
-            Text(text = "Sail South")
-        }
-        Button(onClick = {Direction.value="West"
-            if(Random.nextBoolean()){
-                Toast.makeText(context,"Treasure Found!",Toast.LENGTH_SHORT).show()
-                TreasureCount.value+=1
-            }
-            else{
-                Toast.makeText(context,"Ran into a Storm!",Toast.LENGTH_SHORT).show()
-            }}) {
-            Text(text = "Sail North")
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TreasureHuntPreview() {
     TreasureSailTheme {
         TreasureHunt()
     }
--- UnitConvertor/app/src/main/java/com/example/unitconvertor/MainActivity.kt
+++ UnitConvertor/app/src/main/java/com/example/unitconvertor/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -31,7 +32,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconvertor.ui.theme.UnitConvertorTheme
@@ -71,7 +71,7 @@ fun UnitConverter(){
         outputValue=result.toString()
     }
 
-    Column(modifier = Modifier.fillMaxSize(),verticalArrangement= Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = Modifier.fillMaxSize().offset(y=(-100.dp)),verticalArrangement= Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "Unit Convertor", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
