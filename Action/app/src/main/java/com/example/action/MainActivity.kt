eff4e5c9b1824163a8526947b35aac7b9c887ec9
Prakhyat Singh <professionalgamersss@gmail.com>
2024-11-06 16:46:41 +0530

    minor changes

--- Action/app/src/main/java/com/example/action/MainActivity.kt
+++ Action/app/src/main/java/com/example/action/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.action
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
